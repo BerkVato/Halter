6ddfd5dd84395f3a9dcf5d20c44baf63edf58048
Eray Gürdereoğlu <76774953+ErayGurdereoglu@users.noreply.github.com>
2022-12-14 22:02:40 +0300

    screen layout

--- halter/lib/responsive/mobile_screen_layout.dart
+++ halter/lib/responsive/mobile_screen_layout.dart
@@ -1,12 +1,42 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:halter/utils/colors.dart';
 
-class MobileScreenLayout extends StatelessWidget {
-  const MobileScreenLayout({super.key});
+class MobileScreenLayout extends StatefulWidget {
+  const MobileScreenLayout({Key? key}) : super(key: key);
+
+
+  @override
+  State<MobileScreenLayout> createState() =>  _MobileScreenLayoutState();
+}
+
+class  _MobileScreenLayoutState extends State<MobileScreenLayout>{
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(child: Text('asdasa')),
+      body: const Center(
+        child: Text('Welcome To Halter')
+        ),
+        bottomNavigationBar: CupertinoTabBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: '',
+              backgroundColor: primaryColor,
+              ),
+              BottomNavigationBarItem(
+              icon: Icon(Icons.sports),
+              label: '',
+              backgroundColor: primaryColor,
+              ),
+              BottomNavigationBarItem(
+              icon: Icon(Icons.account_box),
+              label: '',
+              backgroundColor: primaryColor,
+              ),
+          ],
+          ),
     );
   }
 }
