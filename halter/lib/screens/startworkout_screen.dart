3ff7335516292ec06bd0b7c49bdc7c4e5dd28cf6
Eray Gürdereoğlu <76774953+ErayGurdereoglu@users.noreply.github.com>
2023-01-06 21:31:24 +0300

    Update startworkout_screen.dart

--- halter/lib/screens/startworkout_screen.dart
+++ halter/lib/screens/startworkout_screen.dart
@@ -18,61 +18,9 @@ class _StartWorkoutScreenState extends State<StartWorkoutScreen> {
         title: const Text('Start an Empty Workout'),
         centerTitle: false,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 16.0,
-            vertical: 16,
-          ),
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 16.0),
-                child: IconButton(
-                  icon: Icon(Icons.close, color: Colors.red, size: 30,),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                ListTile(
-                subtitle: Text(
-                  "Upper Body",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w800,
-                    fontSize: 24,
-                    color: Colors.white,
-                  ),
-                ),
-                trailing: Column(
-                  children: <Widget>[
-                    Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Icon(Icons.access_time, color: Colors.white30
-                        ),
-                        SizedBox(
-                          width: 5,
-                        ),
-                        Text(
-                          "60 mins",
-                          style: TextStyle(
-                            color: Colors.white70,
-                            fontWeight: FontWeight.w600,
-                            fontSize: 16
-                          ),
-                          ),
-                      ],
-                    ),
-                  ],
-                ),
-                ),
-            ],
-          ),
-        ),
+      body:
+      Container(
+        
       ),
     );
   }
